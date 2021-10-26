2a994310d9d3fd9fa03da750f5f2c4c40f7454e8
Milyn <milyng.dsilva@gmail.com>
2021-10-26 14:11:29 +0530

    added a new js file

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-alert('Hello');
-alert('World');
+"use strict"; //if variable is not defined it throws an error
+alert('Apple');
+let fruit = "Mangoo";
+let sum = 0;
+let user= "Admin" , age = 22, message= "Hey there";
+setTimeout (function(){
+    alert("15 sec over");
+},10000);//1000= 1 sec timeout
+alert(fruit)
+
+let a = 10;
+a=11; // val  changed
+let c= 3;
+
+const b= 12;
+b=13; //val remains same
+let x;
+function add (a,b) {
+    return a+b;
+}
+sum = add(10,20)
