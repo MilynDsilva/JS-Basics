9ec27a51aa326092f1faa859da4246bfd1a4e2cc
Milyn <milyng.dsilva@gmail.com>
2021-10-27 11:26:19 +0530

    added lose/strict equity ops

---
+++ 5-equalityops.js
@@ -0,0 +1,11 @@
+//strict equality (both have same type and same value)
+console.log(1===1); //true
+console.log('1'===1); //false
+
+//lose equality
+console.log(1==1); //true 
+console.log('1'==1); //true we have string on left so converts  right one to string
+
+console.log('true'==1); //comparing true with true
+//lose dont care for type but cares only for value
+//mostly we use strict
