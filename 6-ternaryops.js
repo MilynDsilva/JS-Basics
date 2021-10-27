ea9c301327b0ab1d059611fe5f7479dd736e45cc
Milyn <milyng.dsilva@gmail.com>
2021-10-27 11:34:09 +0530

    implemented ternary operator with condition

---
+++ 6-ternaryops.js
@@ -0,0 +1,12 @@
+//customer has more than 100 pts,
+// they are gold customer, else
+//they are a silver customer
+
+
+let points = 150;
+
+let type = points > 100 ? 'gold' : 'silver'// produces boolean
+//ternary with conditional ops ,declaring typeof customer
+
+console.log(type);
+//prints gold is pts = 90 = silver cust
