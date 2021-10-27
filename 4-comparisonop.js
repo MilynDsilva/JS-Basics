89d622e374e9f3a57c754a2ec65cfba47978baf6
Milyn <milyng.dsilva@gmail.com>
2021-10-27 11:12:53 +0530

    added relational/equity operators

---
+++ 4-comparisonop.js
@@ -0,0 +1,11 @@
+let x = 1;
+
+//relational
+console.log(x>0); //true
+console.log(x >=1); //true
+console.log(x<1);//false
+console.log(x <=1);
+
+//equity
+console.log(x === 1);  //t 
+console.log(x !== 1);   //f
