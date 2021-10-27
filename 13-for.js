9622485fbea033652f66d7575fd9edb7d90f59a0
Milyn <milyng.dsilva@gmail.com>
2021-10-27 16:06:04 +0530

    Created a new file

---
+++ 13-for.js
@@ -0,0 +1,7 @@
+//looping 
+//for , while, do while, for in , for of loops
+for (let i=0;i<5;i++){
+    console.log("hellow world",i);
+    if(i % 2 !==0) console.log(i);
+} //initail expression;condition;incrementexpression
+
