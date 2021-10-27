c400ade113ca7a67126f895393892afe49a0c7b7
Milyn <milyng.dsilva@gmail.com>
2021-10-27 09:22:34 +0530

    Created a new object

--- objects.js
+++ objects.js
@@ -1,1 +1,17 @@
 
+let name = "john";
+let age = 30;
+
+let person = {
+    name: 'john',
+    age:25
+};
+
+//dot notation
+person.name ='Mohan';
+console.log('name');
+
+//bracket notation
+person['name'] = 'Mosh';
+//can be used if we donot know name of the opersontill the run time
+
