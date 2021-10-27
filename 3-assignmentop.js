981063ab3f41e2587af58d06ff2f00723f3edf8b
Milyn <milyng.dsilva@gmail.com>
2021-10-27 11:05:59 +0530

    Added a new line

---
+++ 3-assignmentop.js
@@ -0,0 +1,16 @@
+let x = 10; //= is assigning 10 to x
+
+//x++;
+//equ to above x= x+1;
+
+//suppose we want to increment buy lets say 5
+
+x = x +5;
+x +=5;  //equ to above
+
+x =x *3;
+// can be written as x *=3;
+
+
+
+
