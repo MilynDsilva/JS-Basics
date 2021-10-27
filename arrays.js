4463c102af9dc5a3ae47d0c4b88e1439f4e687b6
Milyn <milyng.dsilva@gmail.com>
2021-10-27 09:33:42 +0530

    Made use of .length

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//to story list of objects like list of products , colors
+//js is dynamic
+let selectedColors = ['red','orange']; //array initialized
+selectedColors[2] ='green';
+selectedColors[3] =1;
+console.log(selectedColors[0]); //links and type of objs are dynamic
+console.log(selectedColors.length); // prints length
+
