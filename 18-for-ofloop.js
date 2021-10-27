60ddf51075c4be4f1dc3af5504b3c3ee4ff45b34
Milyn <milyng.dsilva@gmail.com>
2021-10-27 16:44:29 +0530

    Added do for-of loop implementation

---
+++ 18-for-ofloop.js
@@ -0,0 +1,21 @@
+const person = {
+    name: 'john',
+    age:30
+};
+//key holds the name of one of propierts above
+for (let key in person)
+    console.log(key,':',person[key]);
+//in each loop value would be diffeent so we use bracket notation
+
+
+const colors = ['red','blue','green'];
+for (let index in colors)
+    console.log(index, colors[index]); //inex 0
+    //console.log(index*1+1, colors[index]); index 1
+
+for (let color of colors) //need not deal with index can directly access the value
+{
+    console.log(color);
+}
+
+//instead of in we use of
