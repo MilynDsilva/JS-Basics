7a2981fb437bac6d6689543cdc4ce42c01a7c6f8
Milyn <milyng.dsilva@gmail.com>
2021-10-27 14:51:06 +0530

    Added switch case implementation

---
+++ 12-switchcase.js
@@ -0,0 +1,12 @@
+let role= 'moderator';
+
+switch (role) {
+    case 'guest':
+        console.log('Guest user');
+        break;
+    case 'moderator':
+        console.log('Guest moderator');
+        break;
+    default:
+        console.log('Unknown role');
+}
