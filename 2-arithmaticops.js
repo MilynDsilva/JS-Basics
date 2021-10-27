6108784c79df3ddd3ec02f8b979c30bf8688a02a
Milyn <milyng.dsilva@gmail.com>
2021-10-27 11:00:14 +0530

    included inc/dec commands

---
+++ 2-arithmaticops.js
@@ -0,0 +1,20 @@
+let x = 10;
+let y = 20;
+let z = 15;
+let a = 12;
+
+console.log(x+y);//expression +,-,*,/,%
+//x**y = x to the power of y
+//increment
+//++
+console.log(++x);
+//increment first then we see in console 
+console.log(y++); //value is 20 si it prints 20 then gets incremeneted , will print in next console.log
+console.log(y);
+
+console.log("---------------");
+//decrement
+console.log(--z);
+
+console.log(a--); 
+console.log(a);
