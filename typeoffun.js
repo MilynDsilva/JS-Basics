33909db585b4ea6d0f07a217468d2c9eb99ba0b4
Milyn <milyng.dsilva@gmail.com>
2021-10-27 09:51:58 +0530

    added a newfile typeoffun

---
+++ typeoffun.js
@@ -0,0 +1,14 @@
+//performs a task
+function greet2(name, lastname) {
+    console.log('Hello '+name+ '' +lastname);
+}
+//calculates a value
+function square(number) {
+    return number*number;
+}
+//suqare(2);
+let number =square(2); //can use it to initalize a variable
+//2 is initialized to number
+console.log(square(3));
+
+//2 functions calls
