5d98945055e7d537f3964478f4cb1d8e34e8fe10
Milyn <milyng.dsilva@gmail.com>
2021-10-28 11:26:34 +0530

    Created a new file

---
+++ Exercise1-maxof2nums.js
@@ -0,0 +1,13 @@
+function maximum (num1,num2) {
+    if (num1 >num2 ){
+        return num1;
+    }
+    else 
+    {
+        return num2;
+    }
+}
+let sum = maximum(10,20);
+console.log(sum);
+
+// return (a>b) ? a:b;
