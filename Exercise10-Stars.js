ae74471cf9d7f536ed9ef848e5de3eac5d969bb7
Milyn <milyng.dsilva@gmail.com>
2021-10-29 11:44:53 +0530

    added a modification

---
+++ Exercise10-Stars.js
@@ -0,0 +1,14 @@
+showstars(10);
+
+function showstars (rows) {
+    star = '*';
+    holder = '';
+    for(i=0;i<rows;i++){
+        //console.log(star);
+        if (true){
+            //holder = holder + star;
+            holder += star;
+            console.log(holder);
+        }
+    }
+}
