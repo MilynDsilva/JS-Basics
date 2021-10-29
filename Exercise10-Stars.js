ac69d931f97b8f22302a90597dd0d716f3a28d9f
Milyn <milyng.dsilva@gmail.com>
2021-10-29 11:46:02 +0530

    added a modification

--- Exercise10-Stars.js
+++ Exercise10-Stars.js
@@ -5,6 +5,7 @@ function showstars (rows) {
     holder = '';
     for(i=0;i<rows;i++){
         //console.log(star);
+        //for(j=0;j<i;j++)
         if (true){
             //holder = holder + star;
             holder += star;
