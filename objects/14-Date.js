561bb5f5737c882aa20f73b47fd8a067d639dd22
Milyn <milyng.dsilva@gmail.com>
2021-11-01 12:45:21 +0530

    Date Formats

---
+++ objects/14-Date.js
@@ -0,0 +1,13 @@
+const date1 = new Date();//constructor
+const date1 = new Date('May 11 2018 09:00');
+const date2 = new Date(2018,4,11,9,0); //year montth= 0 based 11= dec, 4 for may,11=day,9=hr.0min
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+now.setFullYear(2017);
+
+//now.toDateString()
+//now.toTimeString()
+//now.toISOString();
+
+
