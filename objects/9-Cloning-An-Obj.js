06f3a39fb85df593a26f720d9585e6b954f8ce62
Milyn <milyng.dsilva@gmail.com>
2021-11-01 11:12:25 +0530

    created a new file

--- objects/9-Cloning-An-Obj.js
+++ objects/9-Cloning-An-Obj.js
@@ -15,11 +15,12 @@ console.log(another);
 
 
 ///another way of cloning
-const another = Object.assign({},circle); //src 
+const another = Object.assign({},circle); //copys props from 1 or more src objects into a target obj can combine it
 //const another = Object.assign({color:'yellow'},circle); //src 
 //can add new properties also
 //equvalent to 3 lines line 8 to 11
 
 const another = {...circle }; //spread operator basicaly takes all the properties
-//from the circle and puts here inside brackets { }
+//from the circle and puts here inside brackets { } //basicaly fethes and puts in 
 //simplest way
+console.log(another);
