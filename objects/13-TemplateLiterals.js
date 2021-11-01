3c0f48a962f86b2f95a8fc091e7d44d7729afc78
Milyn <milyng.dsilva@gmail.com>
2021-11-01 12:05:14 +0530

    added new line

---
+++ objects/13-TemplateLiterals.js
@@ -0,0 +1,10 @@
+//obejct {} // literals
+//Boolean true,false
+//String '', ""
+//template literals ``//to write cleaner code
+const name = 'Mohan';
+const another = `This is my 
+first message
+${name}`; //looks similar while we print need not write \n and stuff
+//can add placeholder ising ${name}
+console.log(another);
