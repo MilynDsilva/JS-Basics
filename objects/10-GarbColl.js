b31270a45c7f6dce4bb723a2af2a096729a4096f
Milyn <milyng.dsilva@gmail.com>
2021-11-01 11:19:12 +0530

    created a new file and added ref link for math.js

---
+++ objects/10-GarbColl.js
@@ -0,0 +1,4 @@
+let circle = { }; //memory is auto allocated
+console.log(circle);
+//js has garbge colector finds cars or somthing which are no longer in use
+//it auto deallocated memory
---
+++ objects/11-Math.js
@@ -0,0 +1,7 @@
+Math.random();//gens randomn num
+Math.round(1.9);
+Math.max(1,2,3,4,5)
+
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
