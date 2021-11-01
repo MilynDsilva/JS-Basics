ec12e9ec7e59f8646858e7297f36ea000d738ab2
Milyn <milyng.dsilva@gmail.com>
2021-11-01 10:01:53 +0530

    Added new comments

---
+++ objects/5-ConstructorProp.js
@@ -0,0 +1,29 @@
+let x = {};
+//let x = new Object();
+
+//every object has a constructor property
+//
+
+
+
+
+
+
+
+// function createcircle(radius) {
+//     return {
+//         radius,
+//         draw() { 
+//             console.log('draw');
+//         }
+//     };
+// }
+// const circle1= createcircle(1);
+
+// function Circle(radius){
+//         this.radius = radius,
+//         this.draw = function () {
+//             console.log('draw');
+//         }   
+// }
+// const another= Circle(1);
