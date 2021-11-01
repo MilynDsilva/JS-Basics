e0beff50fd158c8bad1c089e8fb9e3b42cc37bd3
Milyn <milyng.dsilva@gmail.com>
2021-11-01 12:07:10 +0530

    usage of  implemented

--- objects/13-TemplateLiterals.js
+++ objects/13-TemplateLiterals.js
@@ -3,7 +3,7 @@
 //String '', ""
 //template literals ``//to write cleaner code
 const name = 'Mohan';
-const another = `This is my 
+const another = `This is ${2+2} my 
 first message
 ${name}`; //looks similar while we print need not write \n and stuff
 //can add placeholder ising ${name}
