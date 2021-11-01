db7fca0ab76bbc10ceac2a626e2cc13eedb391b4
Milyn <milyng.dsilva@gmail.com>
2021-11-01 11:28:45 +0530

    String functions

---
+++ objects/12-String.js
@@ -0,0 +1,19 @@
+//we have 2 stypes of string in js primitive and obj
+//string primitive
+
+const message = 'New message';
+message.split(' '); //displays 2 items seperated with space
+const message = 'New message \'second messgae'; //usinf \ to display '
+//message.length
+//message.includes('New');//case sensitive
+//message.endsWith('e);
+//m.replace('first','second')
+//.indexOf('New)
+//toUpperCase,trim removes white space, trimleft
+message.
+
+//string object
+const another = new String('hi'); //constructor function do we add new
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
