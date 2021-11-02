4b9fe4469343ecf9d71674887d73c4dffd6a2b06
Milyn <milyng.dsilva@gmail.com>
2021-11-02 11:10:05 +0530

    Learnt the use of splice()

--- Arrays/2-AddingElements.js
+++ Arrays/2-AddingElements.js
@@ -6,5 +6,5 @@ numbers.unshift(0,1,2); //pushes eles to right and adds elements to the begining
 numbers.splice(4,0,'a','b');//can go to a position and reove or add an elements
 //starting position is 4 , 0 is delete counts and a and b are the inserting elements 
 console.log(numbers);
-numbers.splice(-2,1,9,9)
+numbers.splice(-9,5,9,9)//from -9th loc 5 eles are deleted and 9,9 are included in the array
 console.log(numbers);
