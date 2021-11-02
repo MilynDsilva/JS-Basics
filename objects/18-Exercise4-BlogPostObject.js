03ffecb333ab8e2f487af958c3faafbc2f464551
Milyn <milyng.dsilva@gmail.com>
2021-11-02 09:27:58 +0530

    Exercise 18 implementation in progress

---
+++ objects/18-Exercise4-BlogPostObject.js
@@ -0,0 +1,12 @@
+//title, body, author, views,comments(auto,body)
+//islive=t or f
+let post = {
+    title: 'a',
+    body: 'b',
+    author:'c',
+    views :10,
+    comments :[
+        {author:'new a',body:'new body'},
+        {author:'new a',body:'new body'},
+    ],
+};
