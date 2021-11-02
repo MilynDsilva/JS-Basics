1a83ff32f078727e9a8ecd54a8416d80cdaeb49d
Milyn <milyng.dsilva@gmail.com>
2021-11-02 14:35:22 +0530

    Created new file

---
+++ Arrays/6-RemovingElements.js
@@ -0,0 +1,12 @@
+const numbers = [1,2,3,4];
+
+//end
+const last = numbers.pop();
+console.log(numbers,last);
+//begining
+console.log(numbers.shift());
+console.log(numbers);
+//middle
+
+console.log(numbers.splice(1,1)); 
+console.log(numbers);
