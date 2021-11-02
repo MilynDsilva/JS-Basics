c67b70678896daf88c01a3d44155b35915d4f5a7
Milyn <milyng.dsilva@gmail.com>
2021-11-02 17:37:28 +0530

    joing arrays using .join() implemented

---
+++ Arrays/11-JoiningArrays.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3,4];
+
+console.log(numbers.join('||'));
+
+const message = 'This is my first message';
+const part = message.split(' ');
+console.log(part);
+
+const combined = part.join('\\/');
+console.log(combined)
