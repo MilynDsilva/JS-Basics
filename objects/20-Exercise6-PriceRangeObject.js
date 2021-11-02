4b06ad3c566140120ae8e73ac9777c4535ccceec
Milyn <milyng.dsilva@gmail.com>
2021-11-02 10:07:19 +0530

    Created exercise 20 implementation

---
+++ objects/20-Exercise6-PriceRangeObject.js
@@ -0,0 +1,11 @@
+let priceRanges = [
+    {label:'$',tooltip:'Inexpensive',minPerPerson:0,maxPerson:10},
+
+    {label:'$$',tooltip:'Moderate',minPerPerson:11,maxPerson:20},
+    
+    {label:'$$$',tooltip:'Expensive',minPerPerson:21,maxPerson:50}
+];
+
+let restuarants = [
+    {averagePerPerson:5}
+]
