e48ca7f01ea1d6e0e9aaaff41a8ee959bdb9ceec
Milyn <milyng.dsilva@gmail.com>
2021-11-03 17:50:49 +0530

    completed program with new logic

--- Arrays/Exercise2-Includes.js
+++ Arrays/Exercise2-Includes.js
@@ -1,12 +1,10 @@
 const numbers = [1,7,3,4,6,8];
 
-console.log(includes1(numbers,0));
+console.log(includes1(numbers,1));
 
 function includes1(numbers,searchele) {
     for (i=0;i<=numbers.length;i++)
-    {   
         if(numbers[i] === searchele)
             return true; 
         return false;
-    }
 }
