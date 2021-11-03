c2923953415439f55321b1817a5c1a3e7644e74c
Milyn <milyng.dsilva@gmail.com>
2021-11-03 10:38:44 +0530

    Filter(); method

---
+++ Arrays/14-Filtering-an-array.js
@@ -0,0 +1,10 @@
+const numbers = [1,-1,2,3];
+
+// const filtered = numbers.filter(function(value){
+//     return value >=0; //if it matches this criteria adds the eles to new 
+//     //array and returs the new array
+// });
+const filtered = numbers.filter(value => value >=0); //if it matches this criteria adds the eles to new 
+    //array and returs the new array
+
+console.log(filtered);
