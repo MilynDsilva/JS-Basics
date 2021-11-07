bf00d772ff49705a168fa86282789f6928d0577d
Milyn <milyng.dsilva@gmail.com>
2021-11-07 15:11:59 +0530

    fixed a bug

--- Arrays/Exercise3-Except.js
+++ Arrays/Exercise3-Except.js
@@ -1,20 +1,14 @@
 const numbers = [1,2,3,4];
 
-const output = except(numbers,1);
+const output = except(numbers,[1]);
 
 console.log(output);
 
 function except(array, excluded) {
-    //console.log(numbers);
-    const output = [];
-    for (let elemenet of array)  
-        if (elemenet === excluded)
-        {
-            console.log(numbers.indexOf(4));
-            console.log('true');
-        }
-        else {
-            
-            return false;
-        }  
+   const output = [];
+   for (let element of array)
+    if(!excluded.includes(element))
+        output.push(element);
+    return output;
+   
 }
