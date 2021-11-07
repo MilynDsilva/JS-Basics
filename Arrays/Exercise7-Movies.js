f7be88e7e1afac10b1d61bbfec229054f2190ab7
Milyn <milyng.dsilva@gmail.com>
2021-11-07 18:56:05 +0530

    implemented the map and reverse() methods

--- Arrays/Exercise7-Movies.js
+++ Arrays/Exercise7-Movies.js
@@ -4,8 +4,14 @@ const movies = [
     {title: 'c', year:2015, rating:3.5},
     {title: 'd', year:2018, rating:4}
 ]; //array of objects
+
+const titles =
 movies.filter(m =>m.year === 2018 && m.rating >= 4)
 .sort((a,b) => a.rating = b.rating)
+.reverse()
+.map(m => m.title)
+//m is the movie object
 
+console.log(titles);
 
 //sorts in decending order
