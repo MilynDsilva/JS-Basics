7000e9d6460d87676905c6df5f323f056bdcd7f8
Milyn <milyng.dsilva@gmail.com>
2021-11-08 17:56:48 +0530

    added code

--- functions/11-changing-this.js
+++ functions/11-changing-this.js
@@ -5,11 +5,23 @@ const video = {
         //const self = this;
         //replace this with self ,line 8
         //can remove thi from line 10
-        this.tags.forEach(function(tag){
-            console.log(this.title, tag);
-        },this);
+        //this.tags.forEach(function(tag){
+        this.tags.forEach((tag)=>{
+            console.log(this.title, tag); //by defaults this reffernces to global object
+        //},this);
+        //}.bind(this)); //we are in showtag so can use within showtag
+        });
     }
 };
 
 video.showTags();
 //this referces to the obj that is currently executing
+
+// function playVideo() {
+//     console.log(this);
+// }
+
+// playVideo.call({name:'hello'},1,2);
+// playVideo.apply({name:'hello'},[1,2]);
+// playVideo.bind({name:'hello'})(); //set an arg to a given function
+// playVideo(this); //calls the window function
