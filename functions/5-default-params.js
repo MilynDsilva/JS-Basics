c3c8bbc282f93f6bb555d27810ec5f044cc11533
Milyn <milyng.dsilva@gmail.com>
2021-11-08 11:24:16 +0530

    implemented defaults params

---
+++ functions/5-default-params.js
@@ -0,0 +1,22 @@
+// function interest(principal,rate,years) {
+//     return principal *rate/100 *years;
+// }
+
+// console.log(interest(10000,3.5,5));
+
+
+
+// function interest(principal,rate,years) {
+//     rate = rate || 3.5;
+//     years = years || 6;
+//     return principal *rate/100 *years;
+
+// }
+
+// console.log(interest(10000,3.5));
+
+function interest(principal,rate = 3.6,years = 6) {
+    return principal *rate/100 *years;
+}
+
+console.log(interest(10000,3.5));
